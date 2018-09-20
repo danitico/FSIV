2593b53ee408612737d0e5d1e54c11a2cdbadff1
Daniel Ranchal <danitico98@gmail.com>
2018-09-20 15:13:25 +0200

    :)

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,5 @@
 #include "functions.hpp"
+#include <iostream>
 void getHistogram(cv::Mat &image, std::vector<int> & histogram){
    for(int i=0; i<image.rows; i++){
       uchar *ptr=image.ptr<uchar>(i);
@@ -18,7 +19,14 @@ void getCumulativeHistogram(std::vector<int> & original, std::vector<int> & cumu
    }
 }
 void normalize(std::vector<int> cumulative, std::vector<int> & normalizado){
-   for(int i=0; i<normalizado.size(); i++){
-      normalizado[i]=(cumulative[i]-cumulative[0])*(255/(cumulative[255]-cumulative[0]));
-   }
+      float a=0.0;
+      for(int i=0; i<normalizado.size(); i++){
+         a=(float)(cumulative[i]-cumulative[0])/(float)(cumulative[255]-cumulative[0]);
+         // std::cout << a << '\n';
+         // std::cout << 255*a << '\n';
+         normalizado[i]=255*a;
+      }
+      // for(int i=0; i<normalizado.size(); i++){
+      //    std::cout << "c[" << i << "]" << normalizado[i] << '\n';
+      // }
 }
