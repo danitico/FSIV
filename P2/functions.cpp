f48cd92601882eaaefd8e483b7c59df0b8685015
Daniel Ranchal <danitico98@gmail.com>
2018-10-09 11:01:35 +0200

    Ya tengo idea de como hacerlo :)

--- P2/functions.cpp
+++ P2/functions.cpp
@@ -1,6 +1,62 @@
 #include "functions.hpp"
 #include <cmath>
 #include <iostream>
+#include <cstdlib>
+void obtenerSubImagen(cv::Mat image, cv::Mat subimage, int i, int j, int r){
+   int i_arriba=i;//debo de tener en cuenta los pixeles de la fila i
+   int j_izquierda=j-1;
+   int j_derecha=j+1;
+   int i_abajo=i+1;
+   int i_elemento_cero=i-r;
+   int j_elemento_cero=j-r;
+
+   while(contador1 < r){
+      ptr2=image.ptr<float>(i_arriba);
+      contador2=0;
+      j_izquierda=j-1;
+      while(contador2 < r){
+         //hay que pillar el elemento de más arriba y más a la izquierda
+
+         j_izquierda--;
+         contador2++;
+      }
+      contador3=0;
+      j_derecha=j+1;
+
+      while(contador3 < r){
+         // histogram[ptr2[j_derecha]]++;
+         j_derecha++;
+         contador3++;
+      }
+      // histogram[ptr1[i_arriba]]++;
+      i_arriba--;
+      contador1++;
+   }
+
+
+   while(contador4 < r){
+      ptr2=image.ptr<uchar>(i_abajo);
+      contador2=0;
+      j_izquierda=j-1;
+      while(contador2 < r){
+         // histogram[ptr2[j_izquierda]]++;
+         j_izquierda--;
+         contador2++;
+      }
+      contador3=0;
+      j_derecha=j+1;
+
+      while(contador3 < r){
+         // histogram[ptr2[j_derecha]]++;
+         j_derecha++;
+         contador3++;
+      }
+      // histogram[ptr1[i_abajo]]++;
+      i_abajo++;
+      contador4++;
+   }
+
+}
 cv::Mat createBoxFilter(int r){
    cv::Mat filtro(2*r+1, 2*r+1, CV_32FC1);
 
@@ -14,11 +70,15 @@ cv::Mat createBoxFilter(int r){
 }
 void applyFilter(cv::Mat & in, cv::Mat & filtered, cv::Mat & filter){
    int r=(filter.rows - 1)/2;
-   for(int i=r; i<filtered.rows - r; i++){
-      uchar *ptr=filtered.ptr<uchar>(i);
-      std::cout << "hola" << '\n';
-      for(int j=r; j<filtered.cols - r; j++){
-         cv::Mat a=in(cv::Rect(0, 0, filter.rows, filter.cols));
+   // for(int i=r; i<filtered.rows-r; i++){
+   //    uchar *ptr=filtered.ptr<uchar>(i);
+   //    std::cout << "hola" << '\n';
+   //    for(int j=r; j<filtered.cols-r; j++){
+   // cv::Mat a(in, cv::Rect(i, j, r, r));
+   // std::cout << "i: " << i << "j: "<< j <<'\n';
+   // cv::imwrite("hola.png", a);
+   // exit(-1);
+
 
          // for(int k=0; k<filter.rows; k++){
          //
--- P2/functions.hpp
+++ P2/functions.hpp
@@ -1,6 +1,8 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui.hpp>//imread,imshow,namedWindow,waitKey
+void obtenerSubImagen(cv::Mat image, cv::Mat subimage, int i, int j, int r);
 cv::Mat createBoxFilter(int r);
 cv::Mat createGaussianFilter(int r);
 void applyFilter(cv::Mat &in, cv::Mat & filtered, cv::Mat & filter);
